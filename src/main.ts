e56e1a119fc81c804b6a975071a86bf04f6427b3
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-02-12 09:58:19 +0600

    class-transformer class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable global validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
   const config = new DocumentBuilder()
     .setTitle('New Swagger')
-    .setDescription('this a dec')
+    .setDescription('This is a description')
     .setVersion('1.0')
     .addTag('cats')
     .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
